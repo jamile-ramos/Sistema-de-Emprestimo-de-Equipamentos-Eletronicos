63f3983d639e87752dd7103df4975c71ee59748f
Marcos Kauan Santos Ribas <markauansr@gmail.com>
2024-09-05 00:30:46 -0300

    Correção de bugs em empréstimo
    Bug de visualização

--- sistemaEmprestimo/emprestimo/views.py
+++ sistemaEmprestimo/emprestimo/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+from django.utils.timezone import now
 from django.shortcuts import render, redirect
 from .forms import EmprestimoForm, EmprestimoEditForm
 from .models import Emprestimo
@@ -9,7 +11,8 @@ from django.contrib import messages
 @login_required
 def index(request):
     return render(request, 'emprestimo/index.html')
-    
+
+'''
 @login_required
 def add(request):
     equipamentosDisponiveis = Equipamento.objects.filter(status=1)
@@ -44,6 +47,43 @@ def add(request):
         form = EmprestimoForm()
     
     return render(request, 'emprestimo/forms.html', {'form':form})
+'''
+
+@login_required
+def add(request):
+    # Filtrar equipamentos que estão com status 'Disponível' (status = 1)
+    equipamentosDisponiveis = Equipamento.objects.filter(status=1)
+
+    if not equipamentosDisponiveis.exists():
+        messages.warning(request, "Não há equipamentos disponíveis para empréstimo.")
+        return redirect('/emprestimo/list/')
+    
+    if request.method == 'POST':
+        form = EmprestimoForm(request.POST)
+        if form.is_valid():
+            emprestimo = form.save(commit=False)
+            
+            if request.user.is_authenticated:
+                if request.user.is_superuser:
+                    emprestimo.funcionario = None
+                else:
+                    funcionario = Funcionario.objects.get(id=request.user.id)
+                    emprestimo.funcionario = funcionario
+
+            emprestimo.save()
+            
+            equipamentos = form.cleaned_data['equipamentos']
+            emprestimo.equipamentos.set(equipamentos)
+            
+            # Atualizar status dos equipamentos para "Emprestado" (status = 2)
+            for equipamento in equipamentos:
+                equipamento.emprestar()  # Atualiza o status para "Emprestado"
+              
+            return redirect('/emprestimo/list/')
+    else:
+        form = EmprestimoForm()
+    
+    return render(request, 'emprestimo/forms.html', {'form': form})
 
     
 @login_required 
@@ -51,6 +91,7 @@ def detail(request, emprestimoId):
     emprestimo = Emprestimo.objects.get(pk=emprestimoId)
     return render(request, 'emprestimo/detail.html', {'emprestimo':emprestimo})
  
+'''
 @login_required    
 def edit(request, emprestimoId):
     emprestimo = Emprestimo.objects.get(pk=emprestimoId)
@@ -64,6 +105,38 @@ def edit(request, emprestimoId):
         form = EmprestimoEditForm(instance=emprestimo)
     
     return render(request, 'emprestimo/edit.html', {'form': form})
+'''
+@login_required    
+def edit(request, emprestimoId):
+    emprestimo = Emprestimo.objects.get(pk=emprestimoId)
+    
+    if request.method == 'POST':
+        form = EmprestimoEditForm(request.POST, instance=emprestimo)
+        if form.is_valid():
+            emprestimo = form.save(commit=False)
+            
+            # Verifica o status do empréstimo e atualiza o status dos equipamentos
+            equipamentos = emprestimo.equipamentos.all()
+            
+            if emprestimo.status == Emprestimo.Status.ANDAMENTO:
+                # Se o empréstimo está "Em Andamento", todos os equipamentos devem estar "Emprestados"
+                for equipamento in equipamentos:
+                    equipamento.emprestar()  # Atualiza o status para "Emprestado"
+            
+            elif emprestimo.status == Emprestimo.Status.CONCLUIDO:
+                # Se o empréstimo está "Concluído", todos os equipamentos devem ser "Devolvidos"
+                for equipamento in equipamentos:
+                    equipamento.devolver()  # Atualiza o status para "Disponível"
+                    
+            # Salva o empréstimo após atualizar os status dos equipamentos
+            emprestimo.save()
+            messages.success(request, 'Os dados foram alterados com sucesso!')
+            return redirect('/emprestimo/list')
+    else:
+        form = EmprestimoEditForm(instance=emprestimo)
+    
+    return render(request, 'emprestimo/edit.html', {'form': form})
+
 
 @login_required
 def list(request):
@@ -81,13 +154,42 @@ def remove(request, emprestimoId):
     emprestimo.delete()
     return redirect('/emprestimo/list')
 
+'''
 def finish(request, emprestimoId):
     emprestimo = Emprestimo.objects.get(pk=emprestimoId)
     equipamentos = emprestimo.equipamentos.all() 
+    
     for equipamento in equipamentos:
         equipamento.devolver() 
         
-    emprestimo.status = 1
+    # Converte timezone.now() para date para comparar com dataDevolucao
+    if now().date() > emprestimo.dataDevolucao:
+        emprestimo.status = 3  # Status "Atrasado"
+    else:
+        emprestimo.status = 1  # Status "Devolvido"
+    
     emprestimo.save()
-        
+    
+    return redirect('/emprestimo/list')
+'''
+
+def finish(request, emprestimoId):
+    emprestimo = Emprestimo.objects.get(pk=emprestimoId)
+    equipamentos = emprestimo.equipamentos.all() 
+    
+    for equipamento in equipamentos:
+        # Verifica se o equipamento está marcado como emprestado (status 2)
+        if equipamento.status == 2:
+            equipamento.devolver()
+        else:
+            messages.warning(request, f'O equipamento {equipamento.nome} não está emprestado.')
+
+    # Converte timezone.now() para date para comparar com dataDevolucao
+    if now().date() > emprestimo.dataDevolucao:
+        emprestimo.status = 3  # Status "Atrasado"
+    else:
+        emprestimo.status = 1  # Status "Devolvido"
+
+    emprestimo.save()
+
     return redirect('/emprestimo/list')
